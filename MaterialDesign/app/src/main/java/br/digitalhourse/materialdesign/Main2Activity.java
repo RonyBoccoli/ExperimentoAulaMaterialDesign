7fd3e71b1c9b8f173c39ff52eb5e447a30941fc6
deborabastos <deborabastos14@gmail.com>
2020-01-29 22:10:02 -0300

    ProjetoTelas1&2

--- MaterialDesign/app/src/main/java/br/digitalhourse/materialdesign/Main2Activity.java
+++ MaterialDesign/app/src/main/java/br/digitalhourse/materialdesign/Main2Activity.java
@@ -2,12 +2,28 @@ package br.digitalhourse.materialdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+    private Button botaocupom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        botaocupom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Cupom aplicado com Sucesso", Toast.LENGTH_LONG);
+
+            }
+        });
+    }
+
+    public void init(){
+        botaocupom = findViewById(R.id.botaocupom);
     }
 }
